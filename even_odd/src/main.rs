0ba706de7c9176487f467e499b3625798c096077
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-02-14 17:58:05 -0600

    even or odd code

--- even_odd/src/main.rs
+++ even_odd/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::stdin;
+
+fn is_even_or_odd(number: i32) {
+    if number % 2 == 0 {
+        println!("{number} is Even"); 
+    } else {
+        println!("{number} is Odd");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a number:");
+
+    let mut input = String::new();
+
+    stdin().read_line(&mut input).expect("Failed to read input.");
+
+    let number: i32 = input.trim().parse().expect("Please enter a valid number.");
+    
+    is_even_or_odd(number);
+
 }

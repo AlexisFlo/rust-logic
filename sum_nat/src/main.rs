62ff8291906cd124e27fcae66072451b80946421
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-02-24 14:00:03 -0600

    add code to get the sum of first n natural numbers

--- sum_nat/src/main.rs
+++ sum_nat/src/main.rs
@@ -1,3 +1,17 @@
+// Given a number n, find the sum of the first natural numbers.
+
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a number");
+
+    let mut input = String::new();
+
+    stdin().read_line(&mut input).expect("Failed to read input");
+
+    let nat_number: i32 = input.trim().parse().expect("Please enter a valid number");
+
+    let sum = (nat_number * (nat_number + 1)) / 2;
+
+    println!("the sum of the first natural numbers is:  {sum}");
 }

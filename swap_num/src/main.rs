107ef4a8fc7a66d59f050b324785b94658ca586f
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-02-25 11:54:49 -0600

    Using Third Variable

--- swap_num/src/main.rs
+++ swap_num/src/main.rs
@@ -1,3 +1,12 @@
+// Given two numbers a and b, the task is to swap them.
 fn main() {
-    println!("Hello, world!");
+    let a: i32 = 5;
+    let b: i32 = 10;
+    println!("a: {a}, b: {b}");
+
+    let temp = a;
+    let a = b;
+    let b = temp;
+
+    println!("a: {a}, b: {b}");
 }

9c7fb9626aef77c701710eb44dab87f618e1b4cb
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-03-04 14:45:27 -0600

    enter data form keyboard

--- great_less_equ/src/main.rs
+++ great_less_equ/src/main.rs
@@ -1,6 +1,19 @@
+use std::io::stdin;
+
 fn main() {
-    let number1: i32 = 50;
-    let number2: i32 = 40;
+
+    let mut input = String::new();
+    let mut input_1 = String::new();
+
+    println!("Enter the first number");
+    stdin().read_line(&mut input).expect("Failed to read input");
+
+    let number1: i32 = input.trim().parse().expect("Please enter a valid number");
+
+    println!("Now the second number");
+    stdin().read_line(&mut input_1).expect("Failed to read input");
+
+    let number2: i32 = input_1.trim().parse().expect("Please enter a valid number");
 
     if number1 > number2 {
         println!("{number1} is greather than {number2}")

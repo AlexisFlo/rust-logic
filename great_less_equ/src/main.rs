dc279abb6c7c7476911827e753557ffe334c4394
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-03-04 14:38:14 -0600

    first part

--- great_less_equ/src/main.rs
+++ great_less_equ/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let number1: i32 = 50;
+    let number2: i32 = 40;
+
+    if number1 > number2 {
+        println!("{number1} is greather than {number2}")
+    } else if number1 < number2 {
+        println!("{number1} is less than {number2}")
+    } else {
+        println!("both numbers are equals")
+    }
 }

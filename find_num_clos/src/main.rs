48a84ef6c1033b07c5dc033adf3ac95d085f74f0
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-03-24 22:32:44 -0600

    add code to get closes number

--- find_num_clos/src/main.rs
+++ find_num_clos/src/main.rs
@@ -1,3 +1,26 @@
+
+fn closest_number(n: i32, m: i32) -> i32 {
+    // find the quotient
+    let mut closest: i32 = 0;
+    let mut min_difference = i32::MAX;
+    
+    println!("Searching closest number");
+
+    // Check numbers around n
+    for i in (n - m.abs())..=(n + m.abs()) {
+        if i % m == 0 {
+            let difference = (n - i).abs();
+
+            if difference < min_difference || (difference == min_difference && i.abs() > closest.abs()) {
+                closest = i;
+                min_difference = difference;
+            }
+        }
+    }
+    closest
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Números cercanos");
+    println!("Número más cercano a 13 múltiplo de 4: {}", closest_number(13, 4));
 }

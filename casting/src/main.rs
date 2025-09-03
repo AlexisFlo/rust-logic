ebfe588970c5c028966a728249b1e4538292045d
Flodev <floresfernandezalexishr@gmail.com>
2025-09-03 14:51:03 -0600

    Add code to cast string to number and string

--- casting/src/main.rs
+++ casting/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let num = "123";
+    println!("{num}");
+    let num1: u32 = num.parse().expect("Not a number");
+    let sum = num1 + 77;
+    let sumc: String = sum.to_string(); 
+    println!("{sumc}");
 }
